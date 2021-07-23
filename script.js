33598bd1f2b79825c2b768ad84eb05d5a68635b8
ZacchNaa <2016.naaz@gmail.com>
2021-07-23 18:50:07 +0000

    Final Commit

--- script.js
+++ script.js
@@ -1,78 +1,25 @@
-// get user choices
-const getUserChoice = (userInput) => {
-  userInput = userInput.toLowerCase();
+//text in human language
+let input = "Whales are have super intelligence";
+// let input = "a whale of a deal!";
+//an array of vowels
+const vowels = ["a", "e", "i", "o", "u"];
+//array to store found vowels
+let resultArray = [];
 
-  if (
-    userInput === "rock" ||
-    userInput === "paper" ||
-    userInput === "scissors" ||
-    userInput === "bomb"
-  ) {
-    return userInput;
-  } else {
-    console.log("Your choice is not available");
-  }
-};
-
-// get computer choice
-const getComputerChoice = () => {
-  let computerChoice = Math.floor(Math.random() * 3);
-  if (computerChoice === 0) {
-    return "rock";
-  } else if (computerChoice === 1) {
-    return "paper";
-  } else if (computerChoice === 2) {
-    return "scissors";
-  } else {
-    return -1;
-  }
-};
-
-// determining the winner
-const determineWinner = (userChoice, computerChoice) => {
-  if (computerChoice === userChoice) {
-    return "The game was a tie!";
-  }
-
-  if (userChoice === "bomb") {
-    return "You won";
-  }
-
-  if (userChoice === "rock") {
-    if (computerChoice === "paper") {
-      return "The computer won!";
-    } else {
-      return "You won!";
+// compare the input text to the vowels.
+for (let i = 0; i < input.length; i++) {
+  for (let j = 0; j < vowels.length; j++) {
+    //comparing the input letter to every letter in the vowels array.
+    //and push matching vowels into the resultArray
+    if (input[i] === vowels[j]) {
+      resultArray.push(input[i]);
     }
   }
-
-  if (userChoice === "paper") {
-    if (computerChoice === "scissors") {
-      return "The computer won";
-    } else {
-      return "You won";
-    }
+  //double the 'e' and the 'u'
+  if (input[i] === "e" || input[i] === "u") {
+    resultArray.push(input[i]);
   }
+}
 
-  if (userChoice === "scissors") {
-    if (computerChoice === "rock") {
-      return "The computer won";
-    } else {
-      return "You won";
-    }
-  }
-};
-
-// play game
-const playGame = () => {
-  let userChoice = getUserChoice("bomb");
-  let computerChoice = getComputerChoice();
-
-  console.log(userChoice);
-  console.log(computerChoice);
-  console.log(determineWinner(userChoice, computerChoice));
-
-  // determineWinner(userChoice, computerChoice);
-};
-
-console.log(playGame());
+//printing the result
+console.log(resultArray.join("").toUpperCase());
